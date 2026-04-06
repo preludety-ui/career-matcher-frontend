5c8dc5471a1ba9cc9d9f652a6399c1e6e21e552e
prelude <preludety@gmail.com>
2026-04-06 18:16:57 -0400

    feat: Service Worker PWA

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'yelma-v1';
+const urlsToCache = [
+  '/',
+  '/mon-espace',
+  '/pricing',
+  '/temoignages',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
